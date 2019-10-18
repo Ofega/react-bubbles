f821a54a7f85a5d8339463af6fdae049e642a271
ofega <fegaeze@gmail.com>
2019-10-18 08:27:45 +0100

    login form works as expected

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route exact path="/" render={props => <BubblePage {...props} />} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
